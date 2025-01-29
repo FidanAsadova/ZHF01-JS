f8f94b72ca7cea70a674da3356781bdf23ede668
Fidan Asadova <asadovafidan23@gmail.com>
2025-01-29 09:31:13 +0400

    new folder added

---
+++ jsMathMethods/main.js
@@ -0,0 +1,15 @@
+// let num = 34.5;
+
+// console.log(Math.floor(num));
+// console.log(Math.min(34, 45, 23, 67, 102));
+// console.log(Math.max(34, 45, 23, 67, 102));
+// console.log(Math.ceil(45.4));
+
+// console.log(Math.sign(23));
+// console.log(Math.sign(-42));
+// console.log(Math.sign(0));
+
+// let max = 60;
+// let min = 20;
+// console.log(Math.floor(Math.random()*(max - min) + min));
+
